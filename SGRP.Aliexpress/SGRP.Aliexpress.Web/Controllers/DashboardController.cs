3cc8a5ce6d195501bff9a827074933502845f94b
Jason <lemintam.rp@gmail.com>
2020-03-19 00:16:02 +0700

    update dashboard

---
+++ SGRP.Aliexpress/SGRP.Aliexpress.Web/Controllers/DashboardController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SGRP.Aliexpress.Web.Models;
+
+namespace SGRP.Aliexpress.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            Random rnd = new Random();
+
+
+            var lstModel = new List<SimpleReportViewModel>();
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Brazil",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "USA",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Portugal",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Russia",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Ireland",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Germany",
+                Quantity = rnd.Next(10)
+            });
+            var lstModel1 = new List<SimpleReportViewModel>();
+            lstModel1.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Brazil",
+                Quantity = rnd.Next(10)
+            });
+            lstModel1.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "USA",
+                Quantity = rnd.Next(10)
+            });
+            lstModel1.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Portugal",
+                Quantity = rnd.Next(10)
+            });
+            lstModel1.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Russia",
+                Quantity = rnd.Next(10)
+            });
+            lstModel1.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Ireland",
+                Quantity = rnd.Next(10)
+            });
+            lstModel1.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Germany",
+                Quantity = rnd.Next(10)
+            });
+            var t = new List<List<SimpleReportViewModel>>
+            {
+                lstModel,
+                lstModel1
+            };
+
+            return View(t);
+        }
+    }
+}
